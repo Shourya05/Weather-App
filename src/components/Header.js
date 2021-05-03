d8345bb9a992112200dcc614a49772f8560d1301
Shourya Sisodia <shouryasisodia99@outlook.com>
2021-05-03 07:45:27 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
 import { ReactComponent as Logo } from "../assets/Logo.svg";
-
 import "./Header.scss";
 
 export default class Header extends Component {
   render() {
     return (
-      <div>
+      <div className="header">
         <Logo />
       </div>
     );
